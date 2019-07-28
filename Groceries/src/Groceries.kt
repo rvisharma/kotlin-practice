49d7add830a00a8e3a1277d23d6701221022b28a
Ravi Sharma <rvisharma91@gmail.com>
2019-07-28 18:50:34 +0530

    Head first kotlin - Chapter 12 - Higher order functions Completed

--- Groceries/src/Groceries.kt
+++ Groceries/src/Groceries.kt
@@ -13,6 +13,71 @@ fun main() {
         Grocery(name = "Ice cream", category = "Frozen", unit = "Pack", unitPrice = 3.0, quantity = 2)
     )
 
+//    minMaxSum(groceries)
+//    println("=================")
+//    mapFilterForEach(groceries)
+//    println("=================")
+    groupByFold(groceries)
+    println("=================")
+}
+
+fun groupByFold(groceries: List<Grocery>) {
+    groceries.groupBy { it.category }.forEach {
+        println(it.key)
+        it.value.forEach { println("  ${it.name}") }
+    }
+
+    val ints = listOf<Int>(1, 2, 3)
+    val sumOfInts = ints.fold(0) { runningSum, item -> runningSum + item }
+    println("sumOfInts: $sumOfInts")
+
+    val productOfInts = ints.fold(1) { acc, item -> acc * item }
+    println("productOfInts: $productOfInts")
+
+    val names = groceries.fold("") { string, item -> "$string ${item.name}" }
+    println("names: $names")
+
+    val changeFrom50 = groceries.fold(50.0) { change, item ->
+        change - (item.unitPrice * item.quantity)
+    }
+    println("changeFrom50: $changeFrom50")
+
+}
+
+fun mapFilterForEach(groceries: List<Grocery>) {
+
+    val vegetables = groceries.filter { it.category == "Vegetable" }
+    println("Vegetables: $vegetables")
+
+    val notFrozen = groceries.filterNot { it.category == "Frozen" }
+    println("notFrozen: $notFrozen")
+
+    val groceryNames = groceries.map { it.name }
+    println("groceryNames: $groceryNames")
+
+    val halfUnitPrice = groceries.map { it.unitPrice * 0.5 }
+    println("halfUnitPrice: $halfUnitPrice")
+
+    val newPrices = groceries
+        .filter { it.unitPrice > 3.0 }
+        .map { it.unitPrice * 2 }
+    println("newPrices: $newPrices")
+
+    println("Grocery Names: ")
+    groceries.forEach { println(it.name) }
+
+    println("Groceries with unitPrice > 3.0")
+    groceries
+        .filter { it.unitPrice > 3.0 }
+        .forEach { println(it.name) }
+
+    var itemNames = ""
+    groceries.forEach { itemNames += "${it.name} " }
+    println("itemNames: $itemNames")
+}
+
+fun minMaxSum(groceries: List<Grocery>) {
+
     val highestUnitPrice = groceries.maxBy { it.unitPrice * 5 }
     println("highestUnitPrice: $highestUnitPrice")
 
